f992080cb2a8675f92e4a942d68c646180f46eee
Sirmurhel <samuelzee256@gmail.com>
2022-10-13 22:29:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora korpa, 2015-10-19, 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-	return (0);
+	return (1);
 }
