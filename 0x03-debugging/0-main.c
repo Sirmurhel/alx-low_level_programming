30af4d14a2640c1356f4c1238aae98acf800e0fe
Sirmurhel <samuelzee256@gmail.com>
2022-10-20 02:06:57 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
